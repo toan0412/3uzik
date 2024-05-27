8f2b7d7d89c92b79955f70efa4e570e8289aa292
toan0412 <truongtoan00189@gmail.com>
2024-05-27 19:39:55 +0700

    seekbar

--- app/src/main/java/com/example/a3uzik/MyExoplayer.java
+++ app/src/main/java/com/example/a3uzik/MyExoplayer.java
@@ -4,23 +4,47 @@ import android.content.Context;
 import androidx.media3.common.MediaItem;
 import androidx.media3.exoplayer.ExoPlayer;
 import com.example.a3uzik.models.SongModel;
+import java.util.List;
 
 public class MyExoplayer {
     private static ExoPlayer exoPlayer = null;
     private static SongModel currentSong = null;
+    private static List<SongModel> playlist = null;
+    private static int currentIndex = -1;
 
     public static SongModel getCurrentSong() {
         return currentSong;
     }
 
-    public static ExoPlayer getInstance(PlayerActivity playerActivity) {
-        return exoPlayer;
-    }
-
-    public static void startPlaying(Context context, SongModel song) {
+    public static ExoPlayer getInstance(Context context) {
         if (exoPlayer == null) {
             exoPlayer = new ExoPlayer.Builder(context).build();
         }
+        return exoPlayer;
+    }
+
+    public static void setPlaylist(List<SongModel> songs) {
+        playlist = songs;
+    }
+
+    public static SongModel getNextSong() {
+        if (playlist != null && !playlist.isEmpty()) {
+            currentIndex = (currentIndex + 1) % playlist.size();
+            return playlist.get(currentIndex);
+        }
+        return null;
+    }
+
+    public static SongModel getPreviousSong() {
+        if (playlist != null && !playlist.isEmpty()) {
+            currentIndex = (currentIndex - 1 + playlist.size()) % playlist.size();
+            return playlist.get(currentIndex);
+        }
+        return null;
+    }
+
+    public static void startPlaying(Context context, SongModel song) {
+        getInstance(context);
         if (currentSong != song) {
             // New song
             currentSong = song;
--- app/src/main/java/com/example/a3uzik/PlayerActivity.java
+++ app/src/main/java/com/example/a3uzik/PlayerActivity.java
@@ -1,26 +1,35 @@
 package com.example.a3uzik;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
 import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.media3.common.MediaItem;
 import androidx.media3.common.Player;
-import androidx.media3.common.util.Log;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.exoplayer.ExoPlayer;
+
 import com.bumptech.glide.Glide;
 import com.example.a3uzik.databinding.ActivityPlayerBinding;
-import com.example.a3uzik.models.CategoryModel;
 import com.example.a3uzik.models.SongModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class PlayerActivity extends AppCompatActivity {
 
     private ActivityPlayerBinding binding;
-    public static CategoryModel category;
-
     private ExoPlayer exoPlayer;
-    private int currentSongIndex = 0;
+    private FirebaseFirestore db;
+    private boolean isShuffle = false;
+    private Handler handler;
+    private Runnable runnable;
 
     private final Player.Listener playerListener = new Player.Listener() {
         @Override
@@ -30,17 +39,19 @@ public class PlayerActivity extends AppCompatActivity {
 
         @Override
         public void onPlaybackStateChanged(int state) {
-            if (state == Player.STATE_ENDED) {
-                playNextSong();
+            switch (state) {
+                case Player.STATE_ENDED:
+                    playNextSong();
+                    break;
+                case Player.STATE_READY:
+                    long durationMillis = exoPlayer.getDuration();
+                    binding.durationTextView.setText(formatDuration(durationMillis));
+                    break;
             }
         }
 
-    };
 
-    @OptIn(markerClass = UnstableApi.class)
-    private void playNextSong() {
-            //Chuyeenr bai hat
-    }
+    };
 
 
     @OptIn(markerClass = UnstableApi.class)
@@ -50,22 +61,149 @@ public class PlayerActivity extends AppCompatActivity {
         binding = ActivityPlayerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        if (MyExoplayer.getCurrentSong() != null) {
-            SongModel currentSong = MyExoplayer.getCurrentSong();
-            binding.songTitleTextView.setText(currentSong.getTitle());
-            binding.songSubtitleTextView.setText(currentSong.getSubtitle());
-            Glide.with(binding.songCoverImageView.getContext()).load(currentSong.getCoverUrl())
+        db = FirebaseFirestore.getInstance();
+        getAllSongs();
+        playCurrentSong();
+        setupButtonListeners();
+    }
+
+    @OptIn(markerClass = UnstableApi.class)
+    private void getAllSongs() {
+        db.collection("songs").get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<SongModel> playlist = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            SongModel song = document.toObject(SongModel.class);
+                            playlist.add(song);
+                        }
+                        MyExoplayer.setPlaylist(playlist);
+
+                    }
+                });
+    }
+
+
+
+    private void setupButtonListeners() {
+        binding.loopBtn.setOnClickListener(v -> toggleLoop());
+        binding.previousBtn.setOnClickListener(v -> playPreviousSong());
+        binding.pauseBtn.setOnClickListener(v -> togglePlayPause());
+        binding.nextBtn.setOnClickListener(v -> playNextSong());
+        binding.shuffleBtn.setOnClickListener(v -> toggleShuffle());
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser && exoPlayer != null) {
+                    long newPosition = (long) progress * exoPlayer.getDuration() / seekBar.getMax();
+                    exoPlayer.seekTo(newPosition);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    private void toggleLoop() {
+        if (exoPlayer != null) {
+            boolean isLooping = exoPlayer.getRepeatMode() == Player.REPEAT_MODE_ONE;
+            if (isLooping) {
+                exoPlayer.setRepeatMode(Player.REPEAT_MODE_OFF);
+                binding.loopBtn.setImageResource(R.drawable.ic_loop);
+            } else {
+                exoPlayer.setRepeatMode(Player.REPEAT_MODE_ONE);
+                binding.loopBtn.setImageResource(R.drawable.ic_loop_on);
+            }
+        }
+    }
+
+    private void toggleShuffle() {
+        if (exoPlayer != null) {
+            isShuffle = !isShuffle;
+            if (isShuffle) {
+                binding.shuffleBtn.setImageResource(R.drawable.ic_shuffle_on);
+            } else {
+                binding.shuffleBtn.setImageResource(R.drawable.ic_shuffle);
+            }
+        }
+    }
+
+    private void togglePlayPause() {
+        if (exoPlayer != null) {
+            if (exoPlayer.isPlaying()) {
+                binding.pauseBtn.setImageResource(R.drawable.ic_resume);
+                exoPlayer.pause();
+            } else {
+                binding.pauseBtn.setImageResource(R.drawable.ic_pause);
+                exoPlayer.play();
+            }
+        }
+    }
+
+    @OptIn(markerClass = UnstableApi.class)
+    private void playSong(SongModel song) {
+        if (song != null) {
+            MyExoplayer.startPlaying(this, song);
+            binding.songTitleTextView.setText(song.getTitle());
+            binding.songSubtitleTextView.setText(song.getSubtitle());
+            Glide.with(binding.songCoverImageView.getContext()).load(song.getCoverUrl())
                     .circleCrop()
                     .into(binding.songCoverImageView);
             Glide.with(binding.songGifImageView.getContext()).load(R.drawable.media_playing)
                     .circleCrop()
                     .into(binding.songGifImageView);
-
-            exoPlayer = MyExoplayer.getInstance(this);
-            binding.playerView.setPlayer(exoPlayer);
-            binding.playerView.showController();
-            exoPlayer.addListener(playerListener);
         }
+    }
+
+    private void playCurrentSong() {
+        SongModel currentSong = MyExoplayer.getCurrentSong();
+        playSong(currentSong);
+        if (currentSong != null) {
+            exoPlayer = MyExoplayer.getInstance(this);
+            exoPlayer.addListener(playerListener);
+
+            if (exoPlayer.getPlaybackState() == Player.STATE_READY) {
+                binding.durationTextView.setText(formatDuration(exoPlayer.getDuration()));
+            }
+
+            handler = new Handler();
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    if(exoPlayer != null){
+                        long currentPosition = exoPlayer.getCurrentPosition();
+                        long duration = exoPlayer.getDuration();
+                        int progress = (int) (currentPosition * 100 / duration);
+                        binding.seekBar.setProgress(progress);
+                        binding.progressTextView.setText(formatDuration(currentPosition));
+                    }
+                    handler.postDelayed(this, 1000);
+                }
+            };
+            handler.postDelayed(runnable, 0);
+        }
+    }
+
+
+
+    private void playNextSong() {
+        SongModel nextSong = MyExoplayer.getNextSong();
+        playSong(nextSong);
+    }
+
+    private void playPreviousSong() {
+        SongModel previousSong = MyExoplayer.getPreviousSong();
+        playSong(previousSong);
     }
 
 
@@ -74,6 +212,9 @@ public class PlayerActivity extends AppCompatActivity {
         super.onDestroy();
         if (exoPlayer != null) {
             exoPlayer.removeListener(playerListener);
+        }
+        if (handler != null && runnable != null) {
+            handler.removeCallbacks(runnable);
         }
     }
 
@@ -84,4 +225,11 @@ public class PlayerActivity extends AppCompatActivity {
             binding.songGifImageView.setVisibility(View.INVISIBLE);
         }
     }
+
+    private String formatDuration(long milliseconds) {
+        long minutes = (milliseconds / 1000) / 60;
+        long seconds = (milliseconds / 1000) % 60;
+        return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+    }
+
 }
